158b8498750a9c3d0ee5206f20a379afdf6548eb
zeros <luca.perini.illustrations@gmail.com>
2024-02-20 18:51:08 +0100

    Esercizio 1

---
+++ main.js
@@ -0,0 +1,8 @@
+//ESERCIZIO 1
+
+let gattiTotali = 44;
+let gattiFila = Math.floor(44 / 6);
+let gattiResto = 44 % 6;
+let gattiRimanenti = gattiFila - gattiResto;
+
+console.log("Ci sono " + gattiFila + " file di gatti e ne mancano " + gattiRimanenti + " per una nuova fila, con un avanzo di " + gattiResto + ".");
